ba0de75b8fdddfd3abac0452b3f90bacebcf17b7
Chippyer <450799968@qq.com>
2021-07-24 14:44:26 +0800

    测试示例 注册中心

---
+++ register-01/src/main/java/com/chippy/register/RegisterApplication.java
@@ -0,0 +1,20 @@
+package com.chippy.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @title: 注册中心启动类
+ * @author: chippy
+ * @date: 2021-07-24 14:07
+ **/
+@EnableEurekaServer
+@SpringBootApplication
+public class RegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterApplication.class, args);
+    }
+
+}
