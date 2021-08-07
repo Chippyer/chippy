15c37f56641dbb1fa54f5dd533fdc6765c6b1dfc
Chippyer <450799968@qq.com>
2021-08-07 18:59:01 +0800

    oss-7-1

--- chippy-oss/src/main/java/com/chippy/oss/configuration/client/OssClientAutoConfiguration.java
+++ chippy-oss/src/main/java/com/chippy/oss/configuration/client/OssClientAutoConfiguration.java
@@ -3,12 +3,9 @@ package com.chippy.oss.configuration.client;
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
 import com.chippy.oss.client.AliClient;
-import com.chippy.oss.context.OssClientContext;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.annotation.Resource;
 
 /**
  * oss客户端自动配置类
@@ -19,17 +16,12 @@ import javax.annotation.Resource;
 @Configuration
 public class OssClientAutoConfiguration {
 
-    @Resource
-    private OssClientContext ossClientContext;
-
     @Bean
     public AliClient aliClient(AliClientProperties aliClientProperties) {
         final OSS ossClient = new OSSClientBuilder()
             .build(aliClientProperties.getEndpoint(), aliClientProperties.getAccessId(),
                 aliClientProperties.getAccessKey());
-        final AliClient aliClient = new AliClient(ossClient, aliClientProperties);
-        ossClientContext.register(aliClient);
-        return aliClient;
+        return new AliClient(ossClient, aliClientProperties);
     }
 
 }
