8760e7ab9d7a80639cf0db0d50bbd45d48fd07c5
Chippyer <450799968@qq.com>
2021-08-07 16:23:29 +0800

    oss init 6_3

--- chippy-oss/src/main/java/com/chippy/oss/configuration/predicate/OssPredicateAutoConfiguration.java
+++ chippy-oss/src/main/java/com/chippy/oss/configuration/predicate/OssPredicateAutoConfiguration.java
@@ -4,6 +4,7 @@ import com.chippy.oss.predicate.FileSizePredicate;
 import com.chippy.oss.predicate.FileTypePredicate;
 import com.chippy.oss.predicate.OssPredicate;
 import com.chippy.oss.predicate.OssPredicateHandler;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,11 +24,13 @@ public class OssPredicateAutoConfiguration {
     @Resource
     private OssPredicateProperties ossPredicateProperties;
 
+    @ConditionalOnProperty(prefix = "oss.predicate", name = "max-file-size")
     @Bean
     public FileSizePredicate fileSizePredicate() {
         return new FileSizePredicate(ossPredicateProperties);
     }
 
+    @ConditionalOnProperty(prefix = "oss.predicate", name = "type")
     @Bean
     public FileTypePredicate fileTypePredicate() {
         return new FileTypePredicate(ossPredicateProperties);
